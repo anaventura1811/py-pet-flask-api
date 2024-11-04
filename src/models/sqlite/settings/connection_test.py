ba764a38bcb18db69b0602efc03b68f843594d68
Ana Ventura <anaventura1811@gmail.com>
2024-11-04 16:11:33 -0300

    config: add pytest to requirements.txt, add connection to db, add tests to connection to db

---
+++ src/models/sqlite/settings/connection_test.py
@@ -0,0 +1,13 @@
+'''
+    Pesquisar mais design pattern - singletom
+'''
+
+from .connection import db_connection_handler
+
+
+def test_connect_to_db():
+    assert db_connection_handler.get_engine() is None
+    db_connection_handler.connect_to_db()
+    db_engine = db_connection_handler.get_engine()
+
+    assert db_engine is not None
