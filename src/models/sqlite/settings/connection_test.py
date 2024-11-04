99120ba8bf6fbf9c3e0c7ee5b0b6a3e6aa6446e0
Ana Ventura <anaventura1811@gmail.com>
2024-11-04 16:16:58 -0300

    config: add comments to test file

--- src/models/sqlite/settings/connection_test.py
+++ src/models/sqlite/settings/connection_test.py
@@ -1,13 +1,15 @@
 '''
     Pesquisar mais design pattern - singletom
 '''
-
+from sqlalchemy.engine import Engine
 from .connection import db_connection_handler
 
 
+# Teste de integração com banco de dados
 def test_connect_to_db():
     assert db_connection_handler.get_engine() is None
     db_connection_handler.connect_to_db()
     db_engine = db_connection_handler.get_engine()
 
     assert db_engine is not None
+    assert isinstance(db_engine, Engine)
