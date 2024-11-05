eb8e68ece532141bce96f2dfe5c8773d715eb62e
Ana Ventura <anaventura1811@gmail.com>
2024-11-05 19:43:16 -0300

    feat: add unit tests for person creator controller

--- src/controllers/person_creator_controller.py
+++ src/controllers/person_creator_controller.py
@@ -9,7 +9,7 @@ class PersonCreatorController:
         self.__people_repository = people_repository
 
     def create(self, person_info: Dict) -> Dict:
-        first_name = person_info["first_name"]
+        first_name = person_info.get("first_name")
         last_name = person_info["last_name"]
         age = person_info["age"]
         pet_id = person_info["pet_id"]
@@ -20,11 +20,11 @@ class PersonCreatorController:
             last_name=last_name,
             age=age,
             pet_id=pet_id)
-        self.__format_response(person_info)
+        return self.__format_response(person_info)
 
     def __validate_full_name(self, first_name: str, last_name) -> None:
         # Regex para caracteres que não são letras
-        invalid_characters = re.compile(r'[^a-ZA-Z]')
+        invalid_characters = re.compile(r'[^a-z]', flags=re.IGNORECASE)
 
         if invalid_characters.search(
             first_name
@@ -32,12 +32,11 @@ class PersonCreatorController:
                 last_name):
             raise Exception("Nome da pessoa inválido!")
 
-    def __insert_person_in_db(
-            self,
-            first_name: str,
-            last_name: str,
-            age: int,
-            pet_id: int) -> None:
+    def __insert_person_in_db(self,
+                              first_name: str,
+                              last_name: str,
+                              age: int,
+                              pet_id: int) -> None:
         self.__people_repository.insert_person(
             first_name=first_name,
             last_name=last_name,
