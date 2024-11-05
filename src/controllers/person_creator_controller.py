212b13b0d3692a7a3fd6b5b4ab47fd101232b35e
Ana Ventura <anaventura1811@gmail.com>
2024-11-05 16:45:09 -0300

    feat: add person_creator_controller, remove rule from pylint

--- src/controllers/person_creator_controller.py
+++ src/controllers/person_creator_controller.py
@@ -1,3 +1,5 @@
+from typing import Dict
+import re
 import src.models.sqlite.interfaces.people_repository as interface
 
 
@@ -6,5 +8,47 @@ class PersonCreatorController:
                  interface.PeopleRepositoryInterface) -> None:
         self.__people_repository = people_repository
 
-    def get(self):
-        return self.__people_repository
+    def create(self, person_info: Dict) -> Dict:
+        first_name = person_info["first_name"]
+        last_name = person_info["last_name"]
+        age = person_info["age"]
+        pet_id = person_info["pet_id"]
+
+        self.__validate_full_name(first_name=first_name, last_name=last_name)
+        self.__insert_person_in_db(
+            first_name=first_name,
+            last_name=last_name,
+            age=age,
+            pet_id=pet_id)
+        self.__format_response(person_info)
+
+    def __validate_full_name(self, first_name: str, last_name) -> None:
+        # Regex para caracteres que não são letras
+        invalid_characters = re.compile(r'[^a-ZA-Z]')
+
+        if invalid_characters.search(
+            first_name
+            ) or invalid_characters.search(
+                last_name):
+            raise Exception("Nome da pessoa inválido!")
+
+    def __insert_person_in_db(
+            self,
+            first_name: str,
+            last_name: str,
+            age: int,
+            pet_id: int) -> None:
+        self.__people_repository.insert_person(
+            first_name=first_name,
+            last_name=last_name,
+            age=age,
+            pet_id=pet_id)
+
+    def __format_response(self, person: Dict) -> Dict:
+        return {
+            "data": {
+                "type": "Person",
+                "count": 1,
+                "attributes": person,
+            }
+        }
